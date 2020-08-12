3db5cbc6a356b4d60629fff9e49187149096c0d7
Shane Wilson <65076931+shanewils@users.noreply.github.com>
2020-08-12 14:42:29 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyD63gjbQhRi0Is1iqxf-vw_u99txzsacis",
+    authDomain: "film-api-2ccd2.firebaseapp.com",
+    databaseURL: "https://film-api-2ccd2.firebaseio.com",
+    projectId: "film-api-2ccd2",
+    storageBucket: "film-api-2ccd2.appspot.com",
+    messagingSenderId: "472669266294",
+    appId: "1:472669266294:web:5748c78c0c069672085a57",
+    measurementId: "G-NZ2JHP8CNP"
+});
+
+
+export default firebase;
